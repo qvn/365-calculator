a5bd0fec09d4db6aa8303812e13c508104a93199
Quyen Nguyen <quyen.nguyen@clutchanalytics.com>
2019-11-12 23:26:44 -0600

    Updates

---
+++ precache-manifest.0e898251be8a3790fc1d324688dba4e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fb69c06c94b50a38ae6a996f701560",
+    "url": "/365-calculator/index.html"
+  },
+  {
+    "revision": "8fe9eb38b05d285a4fc7",
+    "url": "/365-calculator/static/css/main.0c3ad049.chunk.css"
+  },
+  {
+    "revision": "3ab9053fba553de926fe",
+    "url": "/365-calculator/static/js/2.472edea1.chunk.js"
+  },
+  {
+    "revision": "8fe9eb38b05d285a4fc7",
+    "url": "/365-calculator/static/js/main.44836c64.chunk.js"
+  },
+  {
+    "revision": "2dd6ec20d713d4d7028c",
+    "url": "/365-calculator/static/js/runtime-main.1c9c81d8.js"
+  }
+]);
